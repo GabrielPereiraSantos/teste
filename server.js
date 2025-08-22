2881849dba4166ecdcf9f9d0cf272a24634c82bf
GabrielPereiraSantos <bielpereirasantos2006@gmail.com>
2025-08-22 10:47:27 -0300

    Update server.js

--- server.js
+++ server.js
@@ -31,17 +31,16 @@ app.get('/usuarios', async (req, res) => {
 
 app.put('/usuarios/:id', async (req, res) => {
 
-    console.log(req)
- //   await prisma.user.update({
- //       where: {
- //           id:
- //       },
- //       data: {
- //           email: req.body.email,
- //           name:req.body.name,
- //           age: req.body.age
- //       }
- //   })
+    await prisma.user.update({
+       where: {
+           id: req.params.id
+        },
+       data: {
+            email: req.body.email,
+            name:req.body.name,
+            age: req.body.age
+        }
+    })
 
 res.status(201).json(req.body)
 
