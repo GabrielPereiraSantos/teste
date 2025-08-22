28db528b771320457336a49d024f9a12df5da4f0
tccrdgp <bielpereirasantos2006@gmail.com>
2025-08-21 23:16:10 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ import express from 'express'
 
 const app = express()
 
+const users = []
+
+app.post('/usuarios', (req, res) =>{
+
+console.log(req)
+
+res.send('Ok post')
+
+})
+
 app.get('/usuarios', (req, res) => {
     res.send(`Ok, deu bom`)
 })
 
 app.listen(3000)
+
